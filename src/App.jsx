08bec896695386f5b41929bb15c1a068a973e07c
RohitZe <rohithsharmaa105@gmail.com>
2026-03-16 15:25:51 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
             <li>
               <a href="https://vite.dev/" target="_blank">
                 <img className="logo" src={viteLogo} alt="" />
-                Hellish Dependicies not so much
+                Hellish Dependicies not so much..
               </a>
             </li>
             <li>
