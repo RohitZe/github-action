5ac208de499512a31a6b2cfb43de9e01f2bc4374
RohitZe <rohithsharmaa105@gmail.com>
2026-03-16 15:18:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
             <li>
               <a href="https://vite.dev/" target="_blank">
                 <img className="logo" src={viteLogo} alt="" />
-                EXPLORe NON-VITEs 
+                Hellish Dependicies
               </a>
             </li>
             <li>
